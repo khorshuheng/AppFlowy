d699894f67227b6a5bf2c1862726d837911f7ebd
khorshuheng <solemnpriest@gmail.com>
2024-07-19 10:44:51 +0800

    Remove test

--- frontend/rust-lib/event-integration-test/tests/folder/local_test/mod.rs
+++
@@ -1,6 +0,0 @@
-mod folder_test;
-mod import_test;
-mod script;
-mod subscription_test;
-mod test;
-mod view_publish_test;
--- frontend/rust-lib/event-integration-test/tests/user/migration_test/mod.rs
+++
@@ -1,4 +0,0 @@
-mod document_test;
-mod version_test;
-
-mod collab_db_restore;
